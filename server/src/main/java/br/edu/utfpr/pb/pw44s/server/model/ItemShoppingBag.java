66afaf67f73db7938dde8d37bb672b3c3537e402
Lucas <lucascaladob@gmail.com>
2025-02-12 17:42:19 -0300

    feat: item shopping bag

---
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/model/ItemShoppingBag.java
@@ -0,0 +1,37 @@
+package br.edu.utfpr.pb.pw44s.server.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ItemShoppingBag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @NotNull
+    private Product product;
+
+    private int quantity  = 1;
+
+    @NotNull
+    private double cost;
+
+    @ManyToOne @NotNull
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "shopping_bag_id", nullable = false)
+    @JsonBackReference
+    private ShoppingBag shoppingBag;
+}
