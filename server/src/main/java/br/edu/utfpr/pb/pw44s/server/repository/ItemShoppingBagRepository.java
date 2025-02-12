6e80e086befa99c406242bfbfb54df56fc439330
Lucas <lucascaladob@gmail.com>
2025-02-12 17:42:53 -0300

    feat: item shopping bag

---
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/repository/ItemShoppingBagRepository.java
@@ -0,0 +1,10 @@
+package br.edu.utfpr.pb.pw44s.server.repository;
+
+import br.edu.utfpr.pb.pw44s.server.model.ItemShoppingBag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemShoppingBagRepository extends JpaRepository<ItemShoppingBag, Long> {
+    ItemShoppingBag findByUserId(Long id);
+}
