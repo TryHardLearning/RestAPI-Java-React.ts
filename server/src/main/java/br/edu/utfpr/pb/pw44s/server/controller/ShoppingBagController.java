2d338c39c274e9c77d6d9448265d07bc83cb57e0
Lucas C. Bresolino <lucascaladob@gmail.com>
2025-02-21 21:06:02 -0300

    feat: findOneByUsername Get

--- server/src/main/java/br/edu/utfpr/pb/pw44s/server/controller/ShoppingBagController.java
+++ server/src/main/java/br/edu/utfpr/pb/pw44s/server/controller/ShoppingBagController.java
@@ -7,8 +7,14 @@ import br.edu.utfpr.pb.pw44s.server.service.ICrudService;
 import br.edu.utfpr.pb.pw44s.server.service.IShoppingBagService;
 import br.edu.utfpr.pb.pw44s.server.service.impl.CrudServiceImpl;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("shoppingbag")
@@ -31,5 +37,15 @@ public class ShoppingBagController extends CrudController<ShoppingBag, ShoppingB
     @Override
     protected ModelMapper getModelMapper() {
         return modelMapper;
+    }
+
+    @GetMapping //http://ip.api:port/classname
+    public ResponseEntity<ShoppingBagDTO> findOneByUsername(@RequestParam String username) {
+        ShoppingBag entity = service.findOneByUsername();
+        if ( entity != null) {
+            return ResponseEntity.ok(modelMapper.map(entity, ShoppingBagDTO.class));
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 }
